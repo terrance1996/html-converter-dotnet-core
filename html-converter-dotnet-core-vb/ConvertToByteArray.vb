cee6986917918b76818690283c7f1467445e1229
terrance1996 <johndevis112@outlook.com>
2022-05-11 14:36:11 +0530

    fixed a syntax issue

--- html-converter-dotnet-core-vb/ConvertToByteArray.vb
+++ html-converter-dotnet-core-vb/ConvertToByteArray.vb
@@ -3,7 +3,7 @@ Imports ceTe.DynamicPDF.HtmlConverter
 Public Class ConvertToByteArray
     Friend Shared Sub Run()
         ' Convert HTML to PDF with the default conversion options
-        Dim PdfByteArray As Byte() = Converter.Convert(New Uri("https://www.google.com"));
+        Dim PdfByteArray As Byte() = Converter.Convert(New Uri("https://www.google.com"))
         ' Write the bytes to disk, Or you can do whatever you need to with it
         File.WriteAllBytes("ConvertToByteArray.pdf", PdfByteArray)
     End Sub
